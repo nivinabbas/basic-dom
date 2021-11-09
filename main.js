d239ce7fdf3a6767d6ecfcf7ec387fe2affa3d38
nivinabbas <nivin.abbas1@gmail.com>
2021-11-09 22:13:51 +0200

    Add function and elements to file main

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-// console.log(document)
+const header = document.createElement('h1');
+const subHeader = document.createElement('h2');
+header.innerHTML = 'The Best Game Ever';
+subHeader.innerHTML = 'Created game by Nivin';
+header.style.color = '#c0392b';
+subHeader.style.color = 'green';
+header.style.fontFamily = 'Helvetica';
+subHeader.setAttribute('class', 'sub-header');
+document.body.appendChild(header);
+document.body.appendChild(subHeader);
+
+// console.log(document);
+const playingField = document.getElementById('playing-field');
+// console.log(playingField);
+
+const down = document.getElementById('down');
+// console.log(down);
+
+const ball = document.getElementById('block');
+
+ball.style.backgroundColor = 'yellow';
+
+const moveRight = function () {
+  let num = parseInt('15px'); //15
+  let x = parseInt(document.getElementById('block').style.left) + num || 0;
+  x += 'px';
+  document.getElementById('block').style.left = x;
+};
